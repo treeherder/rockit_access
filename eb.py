701b5e44046647ebaecb66061034e4662eea9cf1
treeherder <b.d.reddybest@gmail.com>
2014-11-05 13:43:06 -0800

    good tuples r good

--- eb.py
+++ eb.py
@@ -2,9 +2,9 @@ from secret import *
 import json
 
 r = eb.user_list_events()
-ret_list ={}
 
-def touch_events():
+def touch_events():  #gets live events 
+  ret_list = {}
   for x in xrange(len(r['events'])):  #iterate over the events
     ret={}
     if (r['events'][x]['event']['status'] == "Live" ):
@@ -14,29 +14,27 @@ def touch_events():
       ret['start'] = r['events'][x]['event']['start_date']
       ret['end'] = r['events'][x]['event']['end_date']
       ret_list[ret['id']] = (ret)
+  return (ret_list)
 
-
-def authorize_schedule():
-  valid_times = ()
+def authorize_schedule(ret_list):  #returns a list of tuples that frame the event 
+  valid_times = []
   for ret in ret_list:
-      #print ret_list[ret] ["start"],
-      #print ret_list[ret] ["end"]
       (start_date,start_time) = json.dumps(ret_list[ret] ["start"]).split(' ')
       start_date = start_date.replace('"', '').strip()
       start_time = start_time.replace('"', '').strip()
-      print( (start_date,start_time) )
-
       (end_date,end_time) = json.dumps(ret_list[ret] ["end"]).split(' ')
       end_date = end_date.replace('"', '').strip()
       end_time = end_time.replace('"', '').strip()
-      print( (end_date,end_time) )
-
+      valid_tuple = ((start_date,start_time),(end_date,end_time) )
+      valid_times.append(valid_tuple)
+  return (valid_times)
 
 
 def authorize_user():
+
   for x in xrange(len(ret_list)):
     person = eb.list_event_attendees({'id':ret_list[x]['id']})
     print person
 
-touch_events()
-authorize_schedule()
+for x in authorize_schedule(touch_events()):
+  print x
