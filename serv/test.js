8d4b14e6247ac4864b31208105a7b3d69ff53a41
brendan <b.d.reddybest@gmail.com>
2015-06-07 01:38:49 -0700

    cleanup

--- serv/test.js
+++
@@ -1,9 +0,0 @@
-var child_process = require('child_process');
-
-child_process.exec('python test.py --help', function (err, stdout, stderr){
-    if (err) {
-        console.log("child processes failed with error code: " +
-            err.code);
-    }
-    console.log(stdout);
-});
